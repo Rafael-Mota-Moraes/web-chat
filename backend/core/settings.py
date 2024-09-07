0a92ec4f5b03ce276cd83cce6e78dab972e1f9c6
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-09-07 14:05:51 -0300

    instalando dependencias no projeto

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
+
 
 # Application definition
 
@@ -37,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Rest Framework
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
